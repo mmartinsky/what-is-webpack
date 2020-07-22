5774b3e101f466969ebeec489342cfb151ed0b38
mmartinsky <michaelmartinsky@yahoo.com>
2020-07-21 21:02:42 -0400

    fix intro link to html

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ function HomePage() {
       <div>
         <img src="https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/e4c48715-874f-447a-a915-c4615f7f1123/webpack-js-opt.png" />
       </div>
-      <Link href="step/1">
+      <Link href="step/html">
         <Button variant="contained" color="primary">
           Begin
         </Button>
